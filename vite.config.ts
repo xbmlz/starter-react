ba85fb555eacab9a113d021ef336f3e80d7f9c7b
xbmlz <997909544@qq.com>
2024-08-28 13:09:19 +0800

    feat: add auto import

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +12,10 @@ export default defineConfig({
   },
   plugins: [
     react(),
+
+    AutoImport({
+      imports: ['react'],
+      dts: 'src/auto-imports.d.ts',
+    })
   ],
 })
