3c23051dcdd08a2ad9775bbc9bac306ea22b69e5
xbmlz <997909544@qq.com>
2024-08-28 11:27:40 +0800

    chore: init commit

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import path from 'node:path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
+  plugins: [
+    react(),
+  ],
+})
